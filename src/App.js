abcc383548e84a6661de7280b9600033d4b3845f
MaiSuiSui <1059916631@qq.com>
2018-08-06 21:06:48 +0800

    ceshi

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>哈哈哈哈</h1>
+          <p>nihao</p>
+          <span>suisui</span>
+      </div>
+    );
+  }
+}
+
+export default App;
