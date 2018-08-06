966a11eecb93ba18fc1e67caa6528e1809018389
MaiSuiSui <1059916631@qq.com>
2018-08-06 21:55:19 +0800

    lin

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     render() {
         return (
             <div className="App">
-                HAHAH
+                iiiiii
             </div>
         );
     }
